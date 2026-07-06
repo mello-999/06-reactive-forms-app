eaefc52077194b68e72bb798a2eb68e4025d52e3
Mauricio Donoso <donosomauricio721@gmail.com>
2026-07-06 14:34:06 -0400

    Cap.179-Listo

---
+++ src/app/country/country.routes.ts
@@ -0,0 +1,14 @@
+import { Routes } from "@angular/router";
+import { CountryPage } from "./pages/country-page/country-page";
+
+
+
+
+export const authRoutes: Routes = [
+
+{
+ path: '',
+ component: CountryPage,
+},
+
+]; 
