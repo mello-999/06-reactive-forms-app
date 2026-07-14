d3314eb0cc4ae790c0f1912790c8c57aa16eeebf
Mauricio Donoso <donosomauricio721@gmail.com>
2026-07-14 17:00:17 -0400

    Cap.187-Listo

--- src/app/reactive/pages/dynamic-page/dynamic-page.ts
+++ src/app/reactive/pages/dynamic-page/dynamic-page.ts
@@ -1,11 +1,30 @@
 import { JsonPipe } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-dynamic-page',
   imports: [
-    JsonPipe
+    JsonPipe, ReactiveFormsModule,
   ],
   templateUrl: './dynamic-page.html',
 })
-export class DynamicPage {}
+export class DynamicPage {
+
+private fb = inject( FormBuilder);
+
+myForm: FormGroup = this.fb.group({
+  name: ['', [Validators.required, Validators.minLength(3)]],
+  favoriteGames: this.fb.array([
+    ['Metal Gear', Validators.required],
+    ['Hunter x hunter', Validators.required],
+  ],
+  Validators.minLength(2)
+),
+});
+
+get favoriteGames() {
+   return this.myForm.get('favoriteGames') as FormArray;
+}
+
+}
