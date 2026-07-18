363378c9dc182ece2693a0f13aad077f521142da
Mauricio Donoso <donosomauricio721@gmail.com>
2026-07-18 01:04:04 -0400

    Cap.188-Listo

--- src/app/reactive/pages/dynamic-page/dynamic-page.ts
+++ src/app/reactive/pages/dynamic-page/dynamic-page.ts
@@ -1,6 +1,7 @@
 import { JsonPipe } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { FormArray, FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormUtils } from '../../../utils/form-utils';
 
 @Component({
   selector: 'app-dynamic-page',
@@ -10,8 +11,8 @@ import { FormArray, FormBuilder, FormGroup, ReactiveFormsModule, Validators } fr
   templateUrl: './dynamic-page.html',
 })
 export class DynamicPage {
-
-private fb = inject( FormBuilder);
+  private fb = inject( FormBuilder);
+  formUtils = FormUtils;
 
 myForm: FormGroup = this.fb.group({
   name: ['', [Validators.required, Validators.minLength(3)]],
@@ -26,5 +27,11 @@ myForm: FormGroup = this.fb.group({
 get favoriteGames() {
    return this.myForm.get('favoriteGames') as FormArray;
 }
+
+// isValidFieldInArray( formArray: FormArray, index: number ) {
+//   return (
+//     formArray.controls[index].errors && formArray.controls[index].touched
+//   );  
+// }
 
 }
--- src/app/utils/form-utils.ts
+++ src/app/utils/form-utils.ts
@@ -1,8 +1,24 @@
-import { Form, FormGroup } from "@angular/forms";
+import { Form, FormArray, FormGroup, ValidationErrors } from "@angular/forms";
 
 
 
 export class FormUtils {
+
+static getTextError(errors: ValidationErrors) {
+  for( const key of Object.keys(errors) ) {
+   switch(key) {
+    case 'required':
+      return 'este campo es requerido';
+
+      case 'minlength':
+        return `Minimo de ${ errors['minlength'].requiredLength } caracteres.`
+
+      case 'min':
+        return `valor minimo de ${ errors['min'].min }`
+   }
+  }
+  return null;
+}
 
    static isValidField( form: FormGroup, FieldName: string ): boolean | null {
      return (
@@ -19,23 +35,24 @@ export class FormUtils {
       
  const errors = form.controls[FieldName].errors ?? {};
 
- for( const key of Object.keys(errors) ) {
-   switch(key) {
-    case 'required':
-      return 'este campo es requerido';
-
-      case 'minlength':
-        return `Minimo de ${ errors['minlength'].requiredLength } caracteres.`
-
-      case 'min':
-        return `valor minimo de ${ errors['min'].min }`
-   }
-   }
-
-   
-     return null;
+  return FormUtils.getTextError(errors);
 }
 
+     static isValidFieldInArray( formArray: FormArray, index: number ) {
+  return (
+    formArray.controls[index].errors && formArray.controls[index].touched
+  );  
+}
 
+  static getFieldErrorInArray(formArray: FormArray, index: number): string | null {
 
+      const control = formArray.at(index);
+
+      if (!control) return null;
+
+      const errors = control.errors ?? {};
+
+      return FormUtils.getTextError(errors)
+
+ } 
 }
