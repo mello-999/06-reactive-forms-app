2eab528c037b419bb4488ebdf63339110738adf7
Mauricio Donoso <donosomauricio721@gmail.com>
2026-07-21 21:52:33 -0400

    Cap.189-Listo

--- src/app/reactive/pages/dynamic-page/dynamic-page.ts
+++ src/app/reactive/pages/dynamic-page/dynamic-page.ts
@@ -1,6 +1,6 @@
 import { JsonPipe } from '@angular/common';
 import { Component, inject } from '@angular/core';
-import { FormArray, FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormArray, FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { FormUtils } from '../../../utils/form-utils';
 
 @Component({
@@ -24,14 +24,30 @@ myForm: FormGroup = this.fb.group({
 ),
 });
 
+newFavorite = new FormControl('', Validators.required);
+// newFavorite = this.fb.control([])
+
+
 get favoriteGames() {
    return this.myForm.get('favoriteGames') as FormArray;
 }
 
-// isValidFieldInArray( formArray: FormArray, index: number ) {
-//   return (
-//     formArray.controls[index].errors && formArray.controls[index].touched
-//   );  
-// }
+onAddToFavorites() {
+  if ( this.newFavorite.invalid ) return;
+  const newGame = this.newFavorite.value;
+
+  this.favoriteGames.push(this.fb.control( newGame, Validators.required ));
+
+  this.newFavorite.reset();
+
+}
+
+onDeleteFavorite(index: number) {
+  this.favoriteGames.removeAt(index);
+}
+  
+onSubmit() {
+  this.myForm.markAllAsTouched();
+}
 
 }
